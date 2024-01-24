ce7efea0e03859034a92639bfb1454b78d064232
Onimae <manufelipearias@gmail.com>
2024-01-25 09:34:59 +1100

    splitting text with propper size and overlap for getting context on every chunk

--- src/langchain/textSplitter.tsx
+++ src/langchain/textSplitter.tsx
@@ -1,15 +1,27 @@
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import loadText from "./textLoader";
+import { Document } from "langchain/document";
+
+const supabaseApiKey = process.env.SUPABASE_API_KEY;
+const supabaseURL = process.env.SUPABASE_URL;
+const openaiApiKey = process.env.OPENAI_API_KEY;
 
 const getSplittedText = async () => {
   try {
     const { text } = await loadText();
     const result = text[0].pageContent;
 
-    const splitter = new RecursiveCharacterTextSplitter();
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 500,
+      chunkOverlap: 50,
+      separators: [" ", "\n"],
+    });
 
-    const docOutput = await splitter.createDocuments([result]);
-    console.log(docOutput);
+    const docOutput = await splitter.splitDocuments([
+      new Document({ pageContent: result }),
+    ]);
+
+    return docOutput;
   } catch (error) {
     console.log(error);
   }
