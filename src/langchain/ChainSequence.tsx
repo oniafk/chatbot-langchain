e2be911f527c3058381537343a4fb12fab9a38f0
Onimae <manufelipearias@gmail.com>
2024-02-06 00:55:37 +1100

    gave more information as context in prompt to LLM for improving answers

--- src/langchain/ChainSequence.tsx
+++ src/langchain/ChainSequence.tsx
@@ -22,8 +22,15 @@ async function CustomerServiceAnswer() {
     as soon you understand the question from the customer, process the context provided.
      You should be friendly and only answer from the context provided and never make up answer.
      if you need to ask for more information, ask the user in a friendly manner.
-     If you don't know the answer, apologise and advise the user to email help@email.com.
-     always speak in a friendly and professional manner like talking to a friend.     
+     If you don't know the answer, apologise and advise the user to email help@email.com, use this also when you have questions related to accounts, like changing passwords or questions related to personal info from the customer.
+     always speak in a friendly and professional manner like talking to a friend.
+     give a short and concise answer to the customer where this you think is the answer where the action that the customer is going to execute has the least effort it can be done.
+     just explain the process to solve the customers problem, you dont need to disclose or explain the company policies.
+     also remember that the customer just wants to know the solution to their problem, so be concise and to the point try to make the answer as short as possible, 
+     and get into the details just if the customer ask for them, this means, if you can give a short answer that uses less than 30 words to the customer is ok; 
+     remember, just give the information that the customer needs to solve their problem. 
+     remember that you are the customer assistant, always ask for more info if  you need it and just if you dont have information on the context provided, reffer to the customer to the email provided.
+     if you dont receive any context for the question, probbably this means that this questions does not concern to your role, so, appologize with the customer that you cant give an answer to its question and reffer to the customer to the email provided.  
      this is the question of your customer: {question}
      and this is the context provided: {context} for given a solution to the customer.
      answer:
@@ -70,7 +77,8 @@ async function CustomerServiceAnswer() {
   ]);
 
   const response = await chain.invoke({
-    question: "How can I return an item that is damaged?",
+    question:
+      "I need to know the number of my credit card that its on your website, can you help me with that?",
   });
 
   console.log(response);
