d5881c617796189fd5991a750ece761122839c90
Onimae <manufelipearias@gmail.com>
2024-04-27 19:00:59 +1000

    created request for bringging payment methods info

---
+++ src/app/dataBaseRequests/paymentMethodInfo.tsx
@@ -0,0 +1,5 @@
+export async function getPaymentMethods() {
+  const response = await fetch("http://localhost:3001/payment-methods");
+  const userInfo = await response.json();
+  return userInfo;
+}
