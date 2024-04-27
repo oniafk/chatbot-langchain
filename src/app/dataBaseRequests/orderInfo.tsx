ebcd8d2a79bfabe1c013a6f29d52c09f74128f0d
Onimae <manufelipearias@gmail.com>
2024-04-28 01:59:12 +1000

    removed console.log and fixed sized for userInterfaceMenu view

--- src/app/dataBaseRequests/orderInfo.tsx
+++ src/app/dataBaseRequests/orderInfo.tsx
@@ -1,6 +1,6 @@
 export async function getOrderInfo() {
   const response = await fetch("http://localhost:3001/all-order-info");
   const data = await response.json();
-  console.log(data);
+
   return data;
 }
--- src/components/UserInterfaceMenu/UserInterfaceMenu.tsx
+++ src/components/UserInterfaceMenu/UserInterfaceMenu.tsx
@@ -1,12 +1,14 @@
 import { Separator } from "@/components/ui/separator";
 import { ShowUserInfo } from "./UserInfoView";
+import { PaymentMethodsView } from "./PaymentMethodView";
+import { OrderView } from "./OrdersView";
 
 const UserInterfaceMenu = () => {
   return (
     <div className="w-screen h-screen grid place-items-center ">
       <section className="bg-slate-100 w-4/5 h-3/5 rounded-md max-w-3xl">
         <div className="w-full h-full py-2 px-3">
-          <div>
+          <div className="h-auto">
             <h3 className=" text-gray-900 text-xl font-bold mb-2">
               Project Description
             </h3>
@@ -15,7 +17,7 @@ const UserInterfaceMenu = () => {
             </p>
           </div>
           <Separator className="bg-slate-900" />
-          <div className="mt-2 h-full">
+          <div className="mt-2 h-5/6">
             <section className="flex flex-row h-full items-center space-x-1 text-sm">
               <aside className="flex flex-col text-gray-900 text-sm gap-3 h-full w-auto p-1 font-medium ">
                 <a
@@ -37,9 +39,9 @@ const UserInterfaceMenu = () => {
                   Payment methods
                 </a>
               </aside>
-              <Separator orientation="vertical" className="bg-slate-900 " />
-              <div className=" text-gray-900 h-full grid p-2 ">
-                <ShowUserInfo />
+              <Separator orientation="vertical" className="bg-slate-700 " />
+              <div className=" text-gray-900 h-full grid p-2 w-full">
+                <OrderView />
               </div>
             </section>
           </div>
