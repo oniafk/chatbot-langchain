920cece00a6dcc4bcc6d324f0dda1dc797a72a94
Onimae <manufelipearias@gmail.com>
2024-04-29 03:06:52 +1000

    updated enpoint for query

--- src/app/api/message/route.tsx
+++ src/app/api/message/route.tsx
@@ -1,4 +1,5 @@
 import { MessageArraySchema } from "@/lib/schemas/message.schema";
+import { customerAssistantResponse } from "@/langchain/conversationSequence/retrievalChain";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
@@ -7,18 +8,7 @@ export async function POST(req: Request) {
 
   const lastMessage = parsedMessages[parsedMessages.length - 1];
 
-  // Enviar la solicitud POST a la URL deseada en el servidor local
-  const url = "http://localhost:3000/chatbot"; // Reemplaza con la URL correcta
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ lastMessage }), // Puedes enviar los datos en el cuerpo de la solicitud
-  };
-
-  const serverResponse = await fetch(url, requestOptions);
-  const serverResponseText = await serverResponse.text();
+  const serverResponseText = await customerAssistantResponse(lastMessage.text); // Reemplaza con serverResponse.text(
 
   const llmResponse = new Response(serverResponseText, {
     headers: {
