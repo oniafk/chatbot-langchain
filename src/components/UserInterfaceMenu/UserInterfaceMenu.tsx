821a63d7778bd1a9d459e22337c7f2863026af29
Onimae <manufelipearias@gmail.com>
2024-05-04 12:32:16 +1000

    moddified interface view for getting user info on fetch and handeling changes of the view when customer chose different options

--- src/components/UserInterfaceMenu/UserInterfaceMenu.tsx
+++ src/components/UserInterfaceMenu/UserInterfaceMenu.tsx
@@ -1,9 +1,63 @@
+"use client";
+
 import { Separator } from "@/components/ui/separator";
 import { ShowUserInfo } from "./UserInfoView";
 import { PaymentMethodsView } from "./PaymentMethodView";
 import { OrderView } from "./OrdersView";
+import { useEffect, useState } from "react";
+import {
+  order,
+  paymentMethod,
+  userInfo,
+} from "../../interfaces/userInfoInterfaces";
 
 const UserInterfaceMenu = () => {
+  const [currentView, setCurrentView] = useState("OrderView");
+  const [data, setData] = useState<{
+    orders: order[];
+    payments: paymentMethod[];
+    user: userInfo[];
+  } | null>(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const responseOrders = await fetch(
+        "http://localhost:3001/all-order-info"
+      );
+      const responsePayments = await fetch(
+        "http://localhost:3001/payment-methods"
+      );
+      const responseUser = await fetch("http://localhost:3001/users");
+
+      const dataOrders = await responseOrders.json();
+      const dataPayments = await responsePayments.json();
+      const dataUser = await responseUser.json();
+
+      const data = {
+        orders: dataOrders.orders,
+        payments: dataPayments.paymentMethods,
+        user: dataUser.users,
+      };
+
+      console.log(data, "data");
+      setData(data);
+    };
+    fetchUserData();
+  }, []);
+
+  const changeComponentStatus = () => {
+    switch (currentView) {
+      case "OrderView":
+        return <OrderView orders={data?.orders ?? []} />;
+      case "PaymentMethodsView":
+        return <PaymentMethodsView payments={data?.payments ?? []} />;
+      case "UserInfo":
+        return <ShowUserInfo user={data?.user ?? []} />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="w-screen h-screen grid place-items-center ">
       <section className="bg-slate-100 w-4/5 h-3/5 rounded-md max-w-3xl">
@@ -21,27 +75,39 @@ const UserInterfaceMenu = () => {
             <section className="flex flex-row h-full items-center space-x-1 text-sm">
               <aside className="flex flex-col text-gray-900 text-sm gap-3 h-full w-auto p-1 font-medium ">
                 <a
-                  href=""
+                  href="#"
                   className="hover:bg-gray-300 rounded-md  w-full h-auto py-2 px-3"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setCurrentView("UserInfo");
+                  }}
                 >
                   User Info
                 </a>
                 <a
-                  href=""
+                  href="#"
                   className="hover:bg-gray-300 rounded-md  w-full h-auto py-2 px-3"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setCurrentView("OrderView");
+                  }}
                 >
                   Orders
                 </a>
                 <a
-                  href=""
+                  href="#"
                   className="hover:bg-gray-300 rounded-md  w-full h-auto py-2 px-3"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setCurrentView("PaymentMethodsView");
+                  }}
                 >
                   Payment methods
                 </a>
               </aside>
               <Separator orientation="vertical" className="bg-slate-700 " />
               <div className=" text-gray-900 h-full grid p-2 w-full">
-                <OrderView />
+                {changeComponentStatus()}
               </div>
             </section>
           </div>
