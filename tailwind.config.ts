0534b681bf2f265d7c548a5830e409fd09e96019
Onimae <manufelipearias@gmail.com>
2024-05-04 12:25:57 +1000

    added configuration for scroll on tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -33,8 +33,13 @@ const config = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
+    variants: {
+      extend: {
+        overflow: ["responsive", "hover", "focus"],
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
